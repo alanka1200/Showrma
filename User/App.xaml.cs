674826d170f95b82f3280fbffffeb5a6c00ac7dc
alanka1200 <91613668+alanka1200@users.noreply.github.com>
2023-04-17 22:52:27 +0300

    Add files via upload

---
+++ User/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using User.Model;
+
+namespace User
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ShavaEntities DB = new ShavaEntities();
+        public static Model.User LoginigUsers;
+    }
+}
---
+++ User/Pages/EmployeePage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace User.Pages
+{
+    /// <summary>
+    /// Interaction logic for EmployeePage.xaml
+    /// </summary>
+    public partial class EmployeePage : Page
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            LVEmployees.ItemsSource = App.DB.Employee.Where(x => x.IsDelete != false).ToList();
+
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (string.IsNullOrWhiteSpace(TBSearch.Text))
+            {
+                LVEmployees.ItemsSource = App.DB.Employee.Where(x => x.IsDelete != false).ToList();
+            }
+            else
+            {
+                LVEmployees.ItemsSource =
+                    App.DB.Employee.Where(x => x.IsDelete != false).Where(
+                        a => a.Name.ToString().Contains(TBSearch.Text.ToLower())).ToList();
+            }
+
+        }
+
+        private void BackBtn_OnClickBackBtn_OnClickBRemove_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new HomePage());
+        }
+    }
+}
+
